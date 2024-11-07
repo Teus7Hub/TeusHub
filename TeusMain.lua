7901079a739aed82b569e8a8f140eaf7361fbbb8
Teus <gabriellucasteus2@gmail.com>
2024-11-06 22:30:10 -0300

    TeusMain.lua

---
+++ TeusMain.lua
@@ -0,0 +1,20 @@
+-- Teus Hub Loading 😈
+-- Ant-Afk ❄️
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+     	game:GetService("VirtualUser"):Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+		wait(1)
+		game:GetService("VirtualUser"):Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+	end)
+ print("Ant-Afk")
+-- List Of Games Script 👾
+local MenuUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/SetUIY/main/One.lua"))()
+MenuUI:WindowCreate("Teus Hub","","MENU SCRIPT")
+MenuUI:ButtonAdd("Blox Fruits Main",function()
+    loadstring(game:HttpGet(""))()
+    MenuUI:WindowDelete()
+end)
+MenuUI:ButtonAdd("Blox Fruits Kaitun",function()
+    loadstring(game:HttpGet(""))()
+    MenuUI:WindowDelete()
+end)
+--open source niggas

1f2feb5d50c209bc2e0485ffcdb35db464c90c5e
Teus <gabriellucasteus2@gmail.com>
2024-12-20 10:52:58 -0300

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -9,5 +9,6 @@ UiLoader.Window = UiLoader.Fluent:CreateWindow(
         MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
     }
 ) 
+print('loaded Fisch')
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Tsuo7/TsuoHub/refs/heads/main/fisch"))()

30d24ca00dea4face26a766e6455f87f43f67a57
Teus <gabriellucasteus2@gmail.com>
2026-01-26 22:00:59 -0300

    Update Teusscripts.lua

--- Teusscripts.lua
+++ Teusscripts.lua
@@ -1,5 +1,5 @@
 local MenuUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/SetUIY/main/One.lua"))()
-MenuUI:WindowCreate("Teus Hub V5", "http://www.roblox.com/asset/?id=17140528880", "dsc.gg/teusscripts")
+MenuUI:WindowCreate("Teus Hub", "http://www.roblox.com/asset/?id=17140528880", "dsc.gg/teusscripts")
 
 MenuUI:ButtonAdd("Blox Fruits Main", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Teus7Hub/TeusHub/refs/heads/main/TeusHub.lua"))()

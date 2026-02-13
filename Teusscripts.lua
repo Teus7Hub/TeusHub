22a984294523bf2d1ae9f6030d3021e8b464342e
Teus <gabriellucasteus2@gmail.com>
2026-02-13 20:22:44 -0300

    Update Teusscripts.lua

--- Teusscripts.lua
+++ Teusscripts.lua
@@ -24,7 +24,7 @@ end)
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
     local vu = game:GetService("VirtualUser")
     vu:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
-    task.wait(1)
+    task.wait(0.5)
     vu:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
 end)
 
